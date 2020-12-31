fac6b349f37054549932403c83de645bb1c64d70
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-12-31 21:08:19 +0900

    feat: add link to assignment page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Link from "next/link";
+import Layout from "components/Layout";
+import Label from "components/Label";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Layout>
+      <Link href="/cart">
+        <a>
+          <Label
+            size="40px"
+            weight="500"
+            value="여기를!!! 누르면!!! 과제 페이지로!!!! 갑니다! 🚀"
+          />
+        </a>
+      </Link>
+    </Layout>
+  );
 };
 
 export default Home;
