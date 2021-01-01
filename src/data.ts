577fe1c1d2b3949e37220a63a8493bca4cfb2ab7
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2021-01-02 00:19:33 +0900

    chore: add dummy data

--- src/data.ts
+++ src/data.ts
@@ -1,27 +1,67 @@
 export interface IOkkotData {
   id: number;
-  name: string;
-  currentCount: number;
+  image_url: string;
+  product_name: string;
+  product_price: number;
+  current_count: number;
   stock: number;
-  price: number;
-  deliveryPrice: number;
 }
 
-export const okkotData = [
+export const okkotData: IOkkotData[] = [
+  {
+    id: 594037,
+    image_url: "https://static.okkot.com/assignment/eucalyptus+black+jack.jpg",
+    product_name: "유칼립투스 - 블랙잭",
+    product_price: 48000,
+    current_count: 1,
+    stock: 12,
+  },
+  {
+    id: 594038,
+    image_url: "https://static.okkot.com/assignment/gerbera+pink.jpg",
+    product_name: "실거베라 - 핑크",
+    product_price: 33400,
+    current_count: 1,
+    stock: 7,
+  },
+  {
+    id: 594039,
+    image_url: "https://static.okkot.com/assignment/didiscus.jpg",
+    product_name: "디디스커스 - 화이트",
+    product_price: 54000,
+    current_count: 1,
+    stock: 5,
+  },
+  {
+    id: 594040,
+    image_url: "https://static.okkot.com/assignment/scabiosa+red.jpg",
+    product_name: "옥스포드 - 레드",
+    product_price: 23000,
+    current_count: 1,
+    stock: 11,
+  },
+];
+
+export interface IDeliveryTypeData {
+  id: number;
+  name: string;
+  delivery_price: number;
+}
+
+export const deliveryTypeData: IDeliveryTypeData[] = [
   {
     id: 1,
-    name: "크리스마스 리스틀",
-    currentCount: 1,
-    stock: 10,
-    price: 4500,
-    deliveryPrice: 5000,
+    name: "직접배송: 판매자가 직접 배송",
+    delivery_price: 3000,
   },
   {
     id: 2,
-    name: "크리스마스 트리 세트",
-    currentCount: 1,
-    stock: 3,
-    price: 111630,
-    deliveryPrice: 5000,
+    name: "픽업: 정해진 시간에 픽업",
+    delivery_price: 0,
+  },
+  {
+    id: 3,
+    name: "택배배송",
+    delivery_price: 5000,
   },
 ];
